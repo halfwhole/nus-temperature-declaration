1fec2feb90cdc93be9fffbda9a3ac041db042d45
halfwhole <limziwei2@gmail.com>
2020-08-20 21:47:11 +0800

    Update temperature format and add check flag

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 A quick little python script I wrote this morning to help with my temperature declaration at NUS.
 
-![Screenshot of using the temperature script](./temperature.png)
+![Screenshot of using the temperature script](./screenshot.png)
 
 ## Usage
 
 The python package `beautifulsoup4` needs to be installed beforehand; you can install it using `pip3 install beautifulsoup4`.
 
-Run the script to declare your temperature as follows:
+To declare your temperature, run `python3 temperature.py <temperature>`.
 
-```sh
-$ python3 temperature.py
-```
+To check your temperature, run `python3 temperature.py -c`.
+
+## Setup (optional)
+
+If you don't want to enter your username and password every time, you can store it in a text file.
+
+Copy `config-example.txt` to `config.txt`, replacing the username and password fields accordingly.
 
 Note: with a few additional steps, this can be modified to automate the temperature declaration process entirely using cron jobs. Please don't do that! It kind of defeats the purpose of temperature declarations in the first place.
--- temperature.py
+++ temperature.py
@@ -1,11 +1,13 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 from datetime import datetime
+from getopt import getopt
 from getpass import getpass
 
 VAFS_URL = "https://vafs.nus.edu.sg/adfs/oauth2/authorize?response_type=code&client_id=97F0D1CACA7D41DE87538F9362924CCB-184318&resource=sg_edu_nus_oauth&redirect_uri=https%3A%2F%2Fmyaces.nus.edu.sg%3A443%2Fhtd%2Fhtd"
-
-TEMP_URL = "https://myaces.nus.edu.sg/htd/htd"
+TEMP_SUBMIT_URL = "https://myaces.nus.edu.sg/htd/htd"
+TEMP_GET_URL = "https://myaces.nus.edu.sg/htd/htd?loadPage=viewtemperature&actionToDo=NUS"
 
 def login(username, password):
     vafsParams = {
@@ -29,7 +31,11 @@ def submitTemperature(jsessionID, temperature):
         'familySymptomsFlag': 'N',
         'temperature': temperature
     }
-    tempResponse = requests.post(TEMP_URL, data=tempParams, cookies={'JSESSIONID': jsessionID})
+    tempResponse = requests.post(TEMP_SUBMIT_URL, data=tempParams, cookies={'JSESSIONID': jsessionID})
+    return tempResponse.text
+
+def getTemperature(jsessionID):
+    tempResponse = requests.get(TEMP_GET_URL, cookies={'JSESSIONID': jsessionID})
     return tempResponse.text
 
 def parseTemperatureTable(tempResponseText):
@@ -52,13 +58,49 @@ def printData(tempData):
     for row in tempData:
         print(spacingFormat.format(*row))
 
-if __name__ == '__main__':
-    username = input('Username (nusstu\e0123456X): ')
+def readIsCheck():
+    optdict, _ = getOptdictArgs()
+    return '-c' in optdict
+
+def readTemperature():
+    if readIsCheck(): return None
+    _, args = getOptdictArgs()
+    if not args:
+        raise Exception('No temperature given')
+    return args[0]
+
+def getOptdictArgs():
+    optlist, args = getopt(sys.argv[1:], 'c')
+    optdict = dict(optlist)
+    return optdict, args
+
+def readUsernamePassword():
+    with open('config.txt', 'r') as f:
+        d = dict(line.strip().split(': ') for line in f.readlines())
+        if 'username' in d and 'password' in d:
+            return d['username'], d['password']
+
+    username = input('Username (nusstu\e0123456): ')
     password = getpass('Password: ')
-    temperature = input('Temperature (°C): ')
-    print('Submitting temperature of %s°C ... ' % temperature, end='')
-    jsessionID = login(username, password)
-    tempResponseText = submitTemperature(jsessionID, temperature)
-    print('done!')
+    return username, password
+
+
+if __name__ == '__main__':
+    isCheck = readIsCheck()
+    try:
+        temperature = readTemperature()
+    except Exception as e:
+        print('Please provide your temperature as an argument.')
+        exit()
+    username, password = readUsernamePassword()
+
+    if isCheck:
+        jsessionID = login(username, password)
+        tempResponseText = getTemperature(jsessionID)
+    else:
+        jsessionID = login(username, password)
+        tempResponseText = submitTemperature(jsessionID, temperature)
+        print('Submitted temperature of %s°C' % temperature)
+
     tempData = parseTemperatureTable(tempResponseText)
     printData(tempData)
